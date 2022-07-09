159d1f660c9688c59aae8517908aebb9f518d718
Mato <omotoso.abdulmatin4@gmail.com>
2022-07-09 10:55:22 -0700

    added a comment:

--- Flask_form/app.py
+++ Flask_form/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+#the route for the signup stage
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
